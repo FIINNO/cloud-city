b2b9361934fe7c02c06f0d4b3dfb8962f0a60ee7
Alfbom27 <alst2104@student.miun.se>
2024-12-05 15:04:15 +0100

    skybox added

--- src/main.js
+++ src/main.js
@@ -1,32 +1,62 @@
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 // Scene setup
 const scene = new THREE.Scene();
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+let camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
+camera.position.set(1200, -250, 4000);
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add a Cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+//Skybox textures
+const frontImg = new THREE.TextureLoader().load("./imgs/skybox/corona_ft.png");
+const backImg = new THREE.TextureLoader().load("./imgs/skybox/corona_bk.png");
+const upImg = new THREE.TextureLoader().load("./imgs/skybox/corona_up.png");
+const downImg = new THREE.TextureLoader().load("./imgs/skybox/corona_dn.png");
+const leftImg = new THREE.TextureLoader().load("./imgs/skybox/corona_lf.png");
+const rightImg = new THREE.TextureLoader().load("./imgs/skybox/corona_rt.png");
+
+// const frontImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_front.png");
+// const backImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_back.png");
+// const upImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_up.png");
+// const downImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_down.png");
+// const leftImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_left.png");
+// const rightImg = new THREE.TextureLoader().load("./imgs/skybox/cloudcity_right.png");
+
+const textureLoaderArray = [frontImg, backImg, upImg, downImg, rightImg, leftImg];
+
+const skyBoxMaterialArray = textureLoaderArray.map(texture => {
+    return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+});
+
+
+
+// Skybox cube
+const skyboxGeometry = new THREE.BoxGeometry(10000,10000,10000);
+let skybox = new THREE.Mesh(skyboxGeometry, skyBoxMaterialArray);
+scene.add(skybox);
+
+animate();
+
+
 
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    
     renderer.render(scene, camera);
 }
-animate();
+
 
