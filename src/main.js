52e55a14610e9fc799d27bda98ce36220920b3f2
Alfbom27 <alst2104@student.miun.se>
2024-12-13 08:49:00 +0100

    added cloud city model and ambient lightning

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 // Scene setup
@@ -11,16 +12,30 @@ let camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHei
 camera.position.set(1200, -250, 4000);
 
 
+// Lightning
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+
+// Model loader
+
+const modelLoader = new GLTFLoader().setPath('./models/cloud_city_model/');
+modelLoader.load('scene.gltf', (gltf) => {
+    const mesh = gltf.scene;
+    mesh.position.set(1000.0, 0.0, -1000.0);
+    mesh.scale.set(0.2,0.2,0.2);
+    scene.add(mesh);
+});
 
 // Renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
-const loader = new THREE.TextureLoader();
-const texture = loader.load("imgs/skybox/cloud-city2_upscaled.png");
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("imgs/skybox/cloud-city2_upscaled.png");
 
 const sphereGeometry = new THREE.SphereGeometry(5000, 60, 40);
 const sphereMaterial = new THREE.MeshBasicMaterial({
