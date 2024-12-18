b3af79de0d39b34915bc6c50c9f85c3017dbf633
Filip Norgren <fino2105@student.miun.se>
2024-12-18 15:21:18 +0100

    extended camera animation path

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ loadingManager.onLoad = () => {
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambientLight);
 
-const sunLight = new THREE.DirectionalLight(0xffffff, 5.0);
+const sunLight = new THREE.DirectionalLight(0xffffff, 6.0);
 sunLight.position.set(-3500,1000.0,5000);
 sunLight.castShadow = true;
 scene.add(sunLight);
@@ -71,7 +71,10 @@ const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+
+
 //const controls = new OrbitControls( camera, renderer.domElement );
+   
 
 const loader = new THREE.TextureLoader();
 const texture = loader.load("imgs/skybox/cloud-city-env.png");
@@ -80,7 +83,6 @@ const sphereGeometry = new THREE.SphereGeometry(5000, 60, 40);
 const sphereMaterial = new THREE.MeshBasicMaterial({
     map: texture,
     side: THREE.DoubleSide,
-    overdraw: 0.5
 }); 
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
@@ -95,8 +97,61 @@ const cameraPathPoints = [
     new THREE.Vector3(-1600, 220, -2400),
     new THREE.Vector3(-1400, 230, -1800),
     new THREE.Vector3(-1200, 240, -1200),   
-    new THREE.Vector3(-1000, 250, -1000),
+    new THREE.Vector3(-1000, 250, -800),
+    new THREE.Vector3(-800, 250, -500),
+    new THREE.Vector3(-600, 250, -400),
+    new THREE.Vector3(-500, 250, -350),
+    new THREE.Vector3(-400, 230, -300),
+    new THREE.Vector3(-300, 220, -250),
+    new THREE.Vector3(-200, 210, -200),
+    new THREE.Vector3(-100, 210, -150),
+    new THREE.Vector3(-0, 190, -150),
+    new THREE.Vector3(50, 180, -175),
+    new THREE.Vector3(150, 170, -200),
+    new THREE.Vector3(250, 150, -225),
+    new THREE.Vector3(350, 130, -250),
+    new THREE.Vector3(450, 100, -275),
+    new THREE.Vector3(550, 70, -300),
+    new THREE.Vector3(650, 40, -350),
+    new THREE.Vector3(700, 0, -400),
+    new THREE.Vector3(750, -20, -450),
+    new THREE.Vector3(800, -40, -500),
+    new THREE.Vector3(825, -60, -550),
+    new THREE.Vector3(850, -80, -575),
+    new THREE.Vector3(875, -100, -600),
+    new THREE.Vector3(925, -120, -650),
+    new THREE.Vector3(950, -140, -700),
+    new THREE.Vector3(1000, -160, -800),
+    new THREE.Vector3(1050, -180, -900),
+    new THREE.Vector3(1100, -200, -1000),
+    new THREE.Vector3(1150, -200, -1200),
+    new THREE.Vector3(1175, -200, -1300),
+    new THREE.Vector3(1200, -190, -1400),
+    new THREE.Vector3(1200, -180, -1500),
+    new THREE.Vector3(1175, -170, -1600),
+    new THREE.Vector3(1150, -150, -1700),
+    new THREE.Vector3(1125, -130, -1800),
+    new THREE.Vector3(1100, -100, -1900),
+    new THREE.Vector3(1075, -50, -2000),
+    new THREE.Vector3(1050, 0, -2100),
+    new THREE.Vector3(1000, 50, -2200),
+    new THREE.Vector3(950, 125, -2300),
+    new THREE.Vector3(900, 200, -2400),
+    new THREE.Vector3(850, 250, -2500),
+    new THREE.Vector3(800, 300, -2600),
+    new THREE.Vector3(725, 350, -2675),
+    new THREE.Vector3(650, 400, -2750),
+    new THREE.Vector3(550, 450, -2825),
+    new THREE.Vector3(450, 500, -2900),
+    new THREE.Vector3(350, 550, -2950),
+    new THREE.Vector3(250, 600, -3000),
+    new THREE.Vector3(150, 650, -3050),
+    new THREE.Vector3(50, 700, -3100),
+    new THREE.Vector3(-2000, 200, -3000),
 ];
+
+//(200, -100, -1200);
+
 
 const cameraPath = new THREE.CatmullRomCurve3(cameraPathPoints); 
 
