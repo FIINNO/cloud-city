e7448bdc69a9c24f27495d7c14ecb6182d85f399
Alfbom27 <alst2104@student.miun.se>
2024-12-19 10:04:40 +0100

    animations relative to the camera's position

--- src/animation-utils.js
+++ src/animation-utils.js
@@ -1,68 +1,91 @@
 import * as THREE from 'three';
 
 
-function isPositionValid(posX, width, posY, height){
-    const widthOffset = width*0.8;
-    const heightOffset = height*0.8;
-    if(posX > (-width-widthOffset) && posX < (width+widthOffset) && posY > (-height-heightOffset) && posY < (height+heightOffset)){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
 
 export function randomCloudCarPosition(cloudCarObject, camera, scene){
     cloudCarObject.scale.set(0.5,0.5,0.5);
     const cloudCityObject = scene.getObjectByName('cloud_city');
-
+    
+    const randomDirection = Math.random() < 0.5 ? "behind" : "right";
+    console.log("Direction: ", randomDirection);
     const nearPlane = camera.near;
-    const randomValueZ = Math.random() * (600-nearPlane+200)+nearPlane+200;
 
-    const height = 2 * Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2) * randomValueZ;
-    const width = height * camera.aspect;
-    console.log("Width and height: ", width, " ", height);
     console.log("Camera position: ", camera.position);
 
-    let randomValueX = (Math.random() - 0.5) * (width * 3);
-    let randomValueY = (Math.random() - 0.5) * (height * 2); 
-    let newPositionX = camera.position.x+randomValueX;
-    let newPositionY = camera.position.y+randomValueY;
-
-    // Spacing to the camera near plane.
-    // const widthOffset = width*0.8;
-    // const heightOffset = height*0.8;
-
-    //Ensure the ship is not spawning in the close area of the xy-plane as the near plane. 
-    // while(newPositionX > (-width-widthOffset) && newPositionX < (width+widthOffset) && newPositionY > (-height-heightOffset) && newPositionY < (height+heightOffset)){
-    //     randomValueX = (Math.random() - 0.5) * (width * 3);
-    //     randomValueY = (Math.random() - 0.5) * (height * 2);
-    //     newPositionX = camera.position.x+randomValueX;
-    //     newPositionY = camera.position.y+randomValueY;
-    // }
-
-    while(isPositionValid(randomValueX, width, randomValueY, height)){
-        randomValueX = (Math.random() - 0.5) * (width * 3);
-        randomValueY = (Math.random() - 0.5) * (height * 2);
-        newPositionX = camera.position.x+randomValueX;
-        newPositionY = camera.position.y+randomValueY;
-    }
-
-
-
-    let randomPosition = new THREE.Vector3(newPositionX, newPositionY, 0);
+    let randomPosition = new THREE.Vector3(0, 0, 0);
     // Cloud car travels in from the right
-    if(randomPosition.x<camera.position.x){
-        cloudCarObject.direction = new THREE.Vector3(1,0,0);
-        randomPosition.z = camera.position.z + camera.near + (Math.random() * 600 + 300);
-        cloudCarObject.rotation.set(0, 0, 0);
+    if(randomDirection === "right"){
+        const randomValueZ = Math.random() * 2000 + 500;
+        const heightFar = 2 * Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2) * randomValueZ;
+        const widthFar = heightFar * camera.aspect;
+        const heightNear = 2 * Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2) * nearPlane;
+        console.log("Width far: ", widthFar);
+        console.log("From the right...");
+        
+        // TODO: Scale by distance from camera. 
+
+        let randomValueX = Math.random() * widthFar + widthFar/2;
+        let randomValueY = (Math.random() - 0.5) * heightFar * 0.8;
+        let newPositionX = randomValueX;
+        let newPositionY = randomValueY;
+        
+        while(newPositionX < widthFar && newPositionY < (-heightNear*2) && newPositionY > (heightNear*2)){
+            randomValueX = Math.random() * widthFar + widthFar/2;
+            randomValueY = (Math.random() - 0.5) * heightFar * 0.8;
+            newPositionX = randomValueX;
+            newPositionY = randomValueY;
+        }
+
+
+        cloudCarObject.direction = new THREE.Vector3(-1,0,0);
+
+        
+        randomPosition.x = newPositionX;
+        randomPosition.y = newPositionY;
+        
+        randomPosition.z = -Math.max(randomValueZ, nearPlane + 500)
+        console.log("Z from the right: ", randomPosition.z);
     }
     // Cloud car travels in from behind 
     else{
-        cloudCarObject.direction = new THREE.Vector3(0,0,1);
-        randomPosition.z = camera.position.z + camera.near - 100;
-        cloudCarObject.rotation.set(0, -Math.PI/2,0);
+        const randomValueZ = Math.random() * 400 + nearPlane + 400;
+        const heightFar = 2 * Math.tan(THREE.MathUtils.degToRad(camera.fov) / 2) * randomValueZ;
+        const widthFar = heightFar * camera.aspect;
+
+        let randomValueX = (Math.random() - 0.5) * (widthFar * 3);
+        let randomValueY = (Math.random() - 0.5) * (heightFar * 2); 
+        let newPositionX = randomValueX;
+        let newPositionY = randomValueY;
+
+        while(newPositionX > (-widthFar) && newPositionX < (widthFar) && newPositionY > (-heightFar) && newPositionY < (heightFar)){
+            randomValueX = (Math.random() - 0.5) * (widthFar * 3);
+            randomValueY = (Math.random() - 0.5) * (heightFar * 2);
+            newPositionX = randomValueX;
+            newPositionY = randomValueY;
+        }
+        cloudCarObject.direction = new THREE.Vector3(0,0,-1);
+        randomPosition.x = newPositionX;
+        randomPosition.y = newPositionY;
+        randomPosition.z = camera.near + 100;
     }
+    
+    // TODO: Update the rotation of the cloud cars. Seems like they need rotation with pi/2
+    // TODO: Fix the y-position of cars coming in from the right. Maybe split up isPositionValid or even better, pass the offsets in the separate if-else above. For cars coming in to the right the height should be inside the viewPlane? 
+    let localDirection = cloudCarObject.direction.clone();
+    const worldDirection = localDirection.clone().applyQuaternion(camera.quaternion).normalize();
+    cloudCarObject.direction = worldDirection;
+
+    const quaternion = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,0,-1), worldDirection);
+    cloudCarObject.quaternion.copy(quaternion);
+
+    console.log(worldDirection, cloudCarObject.direction);
+    
+
+    const randomWorldPosition = randomPosition.clone();
+    console.log("Local pos:", randomWorldPosition);
+    camera.localToWorld(randomWorldPosition);
+    console.log("World pos:", randomWorldPosition);
+    randomPosition = randomWorldPosition;
     cloudCarObject.position.set(randomPosition.x, randomPosition.y, randomPosition.z);
     console.log("Cloud car position: ", cloudCarObject.position);
     return cloudCarObject;
